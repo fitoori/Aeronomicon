d79c8cbc974067d5628acab5c3ae8154be853e06
KuriGohan-Kamehameha <satbajaj@outlook.com>
2024-03-30 14:47:26 -0400

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,1 +1,8 @@
 sudo apt update && sudo apt install -y python3-pip python3-gst-1.0 gir1.2-gst-rtsp-server-1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-ugly libx264-dev python3-opencv && pip3 install transformations pymavlink apscheduler pyserial numba opencv-python
+
+
+cd ~
+git clone https://github.com/AprilRobotics/apriltag.git
+cd apriltag
+cmake .
+sudo make install
