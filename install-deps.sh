e88c8bf548e6935ceafdd0f69255f84e63e30b45
KuriGohan-Kamehameha <satbajaj@outlook.com>
2024-03-30 14:53:14 -0400

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install -y python3-pip python3-gst-1.0 gir1.2-gst-rtsp-server-1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-ugly libx264-dev python3-opencv && pip3 install transformations pymavlink apscheduler pyserial numba opencv-python
+sudo apt update -y && sudo apt-get full-upgrade -y && sudo apt install -y cmake python3 python3-pip python3-gst-1.0 gir1.2-gst-rtsp-server-1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-ugly libx264-dev python3-opencv && pip3 install transformations pymavlink apscheduler pyserial numba opencv-python
 
 
 cd ~
