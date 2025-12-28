84163993e12d895eee3575c276480a44b2937b9c
fitoori <sbajaj@me.com>
2025-12-28 18:36:49 -0500

    Add ardupilot prestart script hook

---
+++ util/ardupilot-prestart.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+/usr/bin/python3 /home/pi/Aeronomicon/util/discord.py \
+    --webhook https://discord.com/api/webhooks/1454669304749621282/8LQlnXje9YyXtImmy7eDvARFAg7tYKqf16vuLiJPkQHW4OFXzJLAPSje7vVAi43jHqop \
+    "I'm alive!"
